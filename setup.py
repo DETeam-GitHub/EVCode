7c5f313f14b559d3fbae4a7d96cb7c0889750572
lidongxun967 <89326292+lidongxun967@users.noreply.github.com>
2023-09-23 04:04:54 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
-filepath = 'README.rst'
+filepath = 'README.md'
 
 setup(
     name='EVCode',
-    version='1.1.0',
+    version='1.2.0',
     author='lidongxun967',
     author_email='debug967@outlook.com',
     description='一个通过SMTP进行邮箱验证码发送的项目',
+    long_description=open(filepath, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
     classifiers=[
